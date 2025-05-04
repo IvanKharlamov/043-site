82d7aac7fac9bc1f7d8528052dbd54177f70a823
SurDno <surdno@yandex.ru>
2025-05-04 05:10:53 +0100

    Revert "Update radio-visualizer.js"
    This reverts commit 7056ccd2a4d34737ac6a2349e8c9b3a0ad8fdd63.

--- radio-visualizer.js
+++ radio-visualizer.js
@@ -60,8 +60,8 @@ window.RadioVisualizer = (function() {
         gl.LUMINANCE, gl.UNSIGNED_BYTE,
         null
       );
-	  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-	  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     }
